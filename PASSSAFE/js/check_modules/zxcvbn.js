940d7a4aa4b60d221f98c2aa439415740e20ba03
bbibbi0425 <bbibbi0425@naver.com>
2025-01-13 21:21:06 +0900

    style: zxcvbn.js 색상 수정

--- PASSSAFE/js/check_modules/zxcvbn.js
+++ PASSSAFE/js/check_modules/zxcvbn.js
@@ -5,7 +5,7 @@ export function checkPasswordStrength(password) {
         return {
             score: result.score,
             strengthLevels: ["Very Weak", "Weak", "Fair", "Strong", "Very Strong"],
-            strengthColors: ["#F37878", "#F99A5F", "#FFEB56", "#B9E179", "#00AB59"],
+            strengthColors: ["#F37878", "#F99A5F", "#FFE100", "#B9E179", "#00AB59"],
             strengthWidths: ["20%", "40%", "60%", "80%", "100%"],
         };
     }
